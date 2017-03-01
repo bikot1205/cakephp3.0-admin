4a80b4763b22ea7e7a646a0a7fff58e8162c4501
prakashw3expert <prakashw3expert@gmail.com>
2017-03-01 19:06:31 +0530

    Removed Beta

--- beta/src/Template/Cell/Layout/room_details.ctp
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-    var roomData = <?php echo $room->user_meta; ?>;
-    var roomDetails = <?php echo json_encode($room); ?>;
-    var accupants = <?php echo $accupants; ?>;
-</script> 
--- beta/vendor/wyrihaximus/minify-html/config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Cake\Core\Configure;
-
-if (!Configure::check('WyriHaximus.MinifyHtml.factory')) {
-    Configure::write('WyriHaximus.MinifyHtml.factory', 'WyriHaximus\HtmlCompress\Factory::constructFastest');
-}
